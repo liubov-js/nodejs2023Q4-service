a267ae707ca814a310b057246eff3e5d7027efff
Liubov Krivtsova <avostvirk@gmail.com>
2023-06-05 10:53:33 +0300

    feat: add in-memory db

---
+++ src/db/db.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '../interfaces/user.interface';
+import { Track } from '../interfaces/track.interface';
+import { Album } from '../interfaces/album.interface';
+import { Artist } from '../interfaces/artist.interface';
+import { Favorites } from '../interfaces/favorites.interface';
+
+@Injectable()
+export class InMemoryDb {
+  artists: Artist[] = [];
+  albums: Album[] = [];
+  tracks: Track[] = [];
+  users: User[] = [];
+  favourites: Favorites = {
+    artists: [],
+    tracks: [],
+    albums: [],
+  };
+}
